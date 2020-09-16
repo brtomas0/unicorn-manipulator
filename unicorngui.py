9aa67a3fba2681dae4a9922f015de9c470a350c7
Brandon <brtomas0@gmail.com>
2020-09-16 17:06:38 -0400

    added xaxis change and started block separation

--- unicorngui.py
+++ unicorngui.py
@@ -5,7 +5,6 @@ import os
 app = appJar.gui("Unicorn Curve Exporter")
 
 
-
 def getInputFiles():
     input_folder = os.path.dirname(os.path.dirname(__file__)) + "\\unicorn-manipulator-data\\input"
     print(input_folder)
@@ -25,7 +24,9 @@ def getOutputFolder(out_folder_name):
 def exportFileCSV(unicorn_file):
     curve_list = [x for x in app.getAllCheckBoxes() if app.getAllCheckBoxes()[x] is True]
     out_file_name = output_folder + "\\" + unicorn_file.file_name + ".csv"
-    unicorn_file.exportCurves(curve_list, out_file_name, app.getEntry("resample_size"))
+    resize = app.getEntry("resample_size")
+    x_unit = app.getRadioButton("x unit")
+    unicorn_file.exportCurves(curve_list, out_file_name, resize, x_unit)
     # print(unicorn_file.file_name)
 
 
@@ -37,7 +38,7 @@ def buttonMakeAllCSV():
 
 
 def makeFileBox():
-    app.setStretch("COLUMN")
+    app.setStretch("both")
     app.addListBox("file_list", [unicornfile_list[x].file_name for x in unicornfile_list])
     # app.setFrameWidth("files",300)
     app.setListBoxWidth("file_list", 50)
@@ -52,6 +53,18 @@ def makeCurveCheckboxes(sample_file):
     # app.setListBoxMulti("curve_list", True)
 
 
+def makePhaseFrame():
+    app.setStretch("column")
+    app.addButton("Update Phases", displayPhases)
+
+    app.setStretch("column")
+    app.setInPadding([0, 10])
+    app.addLabel("PL1", "Phases in all methods:")
+    app.addListBox("similar phases")
+    app.addLabel("PL2", "Phases not in all methods:")
+    app.addListBox("different phases")
+
+
 def getUnicornFiles():
     unicornfile_list = {}
     for file in input_files:
@@ -60,6 +73,34 @@ def getUnicornFiles():
         # print(file, unicorn_file.file_name)
 
     return unicornfile_list
+
+
+def curveOverlay():
+    # app.addLabel("test")
+    pass
+
+
+def displayPhases():
+    # phase_list = {}
+    sim_phase_set = set()
+    dif_phase_set = set()
+    ordered_blocks = []
+    for file in app.getListBox("file_list"):
+        unicornfile_list[file].load()
+        temp_set = set(unicornfile_list[file].blocks)
+        if len(sim_phase_set) == 0:
+            sim_phase_set = temp_set
+            ordered_blocks = unicornfile_list[file].blocks
+        else:
+            dif_phase_set |= (sim_phase_set ^ temp_set )
+            sim_phase_set &= temp_set
+        # for block in unicornfile_list[file].blocks:
+        #     phase_list[block] = None
+        #     print(block)
+
+    app.updateListBox("similar phases", [x for x in ordered_blocks if x in sim_phase_set])
+    app.updateListBox("different phases", dif_phase_set)
+    # app.updateListBox("similar phases", phase_list.keys())
 
 
 def main():
@@ -76,16 +117,25 @@ def main():
     makeFileBox()
     app.stopFrame()
 
-    app.startFrame("curves", row=0, column=1)
+    app.startFrame("phases", row=0, column=1)
+    makePhaseFrame()
+    app.stopFrame()
+
+    app.startFrame("curves", row=0, column=2)
     makeCurveCheckboxes(next(iter(unicornfile_list.values())))
     app.stopFrame()
 
-    app.startFrame("data_export", row=0, column=2)
+    app.startFrame("data_export", row=0, column=3)
     app.setStretch("COLUMN")
 
-    app.addMessage("M1", "Select number of points to downsample to:")
+    app.addLabel("L1", "Select number of points\nto resample to:")
     app.addNumericEntry("resample_size")
+    app.addLabel("L2", "Select x-value:")
+    app.addRadioButton("x unit", "mL")
+    app.addRadioButton("x unit", "CV")
+    # app.addRadioButton("x unit", "min")
     app.addButton("Export all csv's", buttonMakeAllCSV)
+    app.addButton("Overlay Curves", curveOverlay)
     app.stopFrame()
 
     app.go()
--- unicornmanipulator.py
+++ unicornmanipulator.py
@@ -1,9 +1,7 @@
 import os
-import datetime
 import pycorn
 import xml.etree.ElementTree as ET
 import collections
-from scipy import signal
 import numpy
 import appJar
 
@@ -19,26 +17,45 @@ class UnicornFile():
     def __init__(self, zip_file_path):
         self.zip_file_path = zip_file_path
         self.file_name = zip_file_path.split("\\")[-1][:-4] # to remove .zip
-        self.not_loaded = True
+        self.loaded = False
         self.pycorn_file = None
-        self.chrom_1_tree = None
+        self.chrom_1_xml = None
         self.logbook = None
         self.blocks = None
         self.curve_names = None
+        self.col_xml = None
+        self.col_bh = None
+        self.col_id = None
+        self.col_cv = None
 
     def load(self):
-        if self.not_loaded:
+        if not self.loaded:
             self.getFileData()
             self.getLogbook()
             self.getBlocks()
             self.getAvailableCurves()
+            self.getColumnData()
+            self.loaded = True
         return self
 
     def getFileData(self):
         # uses pycorn to extract the Chrom.1.Xml data
         self.pycorn_file = pycorn.pc_uni6(self.zip_file_path)
         self.pycorn_file.load()
-        self.chrom_1_tree = ET.fromstring(self.pycorn_file["Chrom.1.Xml"])
+        self.chrom_1_xml = ET.fromstring(self.pycorn_file["Chrom.1.Xml"])
+
+    def getColumnData(self):
+        # gets data regarding the column dimension. Be careful, not sure if
+        # the binary decoding will always work.
+        column_binary = self.pycorn_file["ColumnTypeData"]["Xml"]
+        column_data = column_binary.decode("utf-8", "ignore").strip()
+        column_data = column_data[column_data.index("<"):]
+        self.col_xml = ET.fromstring(column_data)
+
+        self.col_bh = float(self.col_xml.find("ColumnType/BedHeight").text)
+        self.col_id = float(self.col_xml.find("ColumnType/Hardware/Diameter").text)
+        self.col_cv = float(self.chrom_1_xml.find("Curves/Curve/ColumnVolume").text)
+        # print(self.col_bh, self.col_id, self.col_cv)
 
     def getBlocks(self):
         # logbook entries is a list of xml.etree.ElementTree.Elements
@@ -67,21 +84,24 @@ class UnicornFile():
         return blocks_dict
 
     def getLogbook(self):
-        self.logbook = self.chrom_1_tree.findall("EventCurves/EventCurve[@EventCurveType='Logbook']/Events/Event")
+        self.logbook = self.chrom_1_xml.findall("EventCurves/EventCurve[@EventCurveType='Logbook']/Events/Event")
 
     def getAvailableCurves(self):
         # gets a list of all the available curves that can be extracted
         # stores as a dict with the first component of the array storing the file of the curve
         # and the second storing the curve Element object
         # {curvename: [filename, curve_element, [x-data, y-data]]}
-        curves = self.chrom_1_tree.findall("Curves/Curve")
+        curves = self.chrom_1_xml.findall("Curves/Curve")
         curve_dict = {}
         for curve in curves:
             curve_dict[curve.find("Name").text] = [curve.find("CurvePoints/CurvePoint/BinaryCurvePointsFileName").text, curve, []]
         self.curve_names = curve_dict
         return self.curve_names
 
-    def getCurveData(self, curve_name, resample_size=None):
+    def getCurveData(self, curve_name, x_unit='mL', resize=None):
+        # {curvename: [filename, curve_element, [x-data, y-data]]}
+        # resturns the [x-data, y-data] and also stores it in the self.curve_names dict
+
         if self.curve_names is None:
             self.getAvailableCurves()
         elif curve_name not in self.curve_names:
@@ -90,38 +110,57 @@ class UnicornFile():
                 print("\t" + name)
             return None
 
-        x_values = self.pycorn_file[self.curve_names[curve_name][0]]["CoordinateData.Volumes"]
-        y_values = self.pycorn_file[self.curve_names[curve_name][0]]["CoordinateData.Amplitudes"]
-        # print(len(x_values))
+        if x_unit not in ['mL', 'CV', 'min']:
+            print('not the correct x_unit')
+            x_unit = 'mL'
 
-        if resample_size is None:
+        if x_unit == 'mL':
+            x_values = self.pycorn_file[self.curve_names[curve_name][0]]["CoordinateData.Volumes"]
+
+        elif x_unit == 'CV':
+            temp = self.pycorn_file[self.curve_names[curve_name][0]]["CoordinateData.Volumes"]
+            x_values = [x / self.col_cv for x in temp]
+
+        elif x_unit == "min":
+            print(self.pycorn_file[self.curve_names[curve_name][0]].keys())
+            x_values = self.pycorn_file[self.curve_names[curve_name][0]]["CoordinateData.Times"]
+
+        else:
+            print('This should never happen')
+            raise SystemError
+
+        y_values = self.pycorn_file[self.curve_names[curve_name][0]]["CoordinateData.Amplitudes"]
+
+        if resize is None:
             self.curve_names[curve_name][2] = [x_values, y_values]
             return self.curve_names[curve_name][2]
         else:
-            resample_size = int(resample_size)
-            y_values = signal.resample(y_values, resample_size)
-            x_values = numpy.linspace(x_values[0], x_values[-1], resample_size)
+            resize = int(resize)
+            y_values = resizeArr(y_values, resize)
+            x_values = numpy.linspace(x_values[0], x_values[-1], resize)
             self.curve_names[curve_name][2] = [x_values, y_values]
             return self.curve_names[curve_name][2]
 
-    def exportCurves(self, curve_list, output_file, resample_size=None):
+    def exportCurves(self, curve_list, output_file, resize=None, x_unit='mL'):
+        # takes the list of curves to export, name of export file, the risizing amount, and the units to have
+        # the x data in, and exports it to the output file, aligned to the x_axis
         curve_data = []
         header_string = ""
         for curve_name in curve_list:
-            vals = self.getCurveData(curve_name, resample_size)
+            x_values, y_values = self.getCurveData(curve_name, resize=resize, x_unit=x_unit)
 
             if curve_data == []:
-                header_string += "x-values"
-                curve_data.append(vals[0])
-            elif len(vals[0]) < len(curve_data[0]) and resample_size is None:
+                header_string += "x-values(%s)" % x_unit
+                curve_data.append(x_values)
+            elif len(x_values) < len(curve_data[0]) and resize is None:
                 # check to see if the newly added data has fewer points. If yes, set that as the new x-axis
                 # and downsize all the y-data for the various curves
-                curve_data[0] = vals[0]
+                curve_data[0] = x_values
                 for i in range(1, len(curve_data)):
-                    curve_data[i] = signal.resample(curve_data[i], len(vals[0]))
+                    curve_data[i] = resizeArr(curve_data[i], len(x_values))
 
             header_string += "," + curve_name
-            curve_data.append(vals[1])
+            curve_data.append(y_values)
 
         # print(header_string)
         numpy.savetxt(  output_file,
@@ -130,6 +169,12 @@ class UnicornFile():
                         header=header_string,
                         comments="",
                         fmt='%.4f')
+
+
+####################################################################################################################
+####################################################################################################################
+####################################################################################################################
+####################################################################################################################
 
 
 def getFollowingWord(string, keyword, end=" "):
@@ -146,7 +191,7 @@ def getFollowingWord(string, keyword, end=" "):
     return string[start_i:end_i].replace(".", "")
 
 
-def getFiles():
+def getInputFiles():
     input_folder = os.path.dirname(os.path.dirname(__file__)) + "\\unicorn-manipulator-data\\input"
     print(input_folder)
     input_files = os.listdir(input_folder)
@@ -162,52 +207,68 @@ def getOutputFolder(out_folder_name):
     return root_dir + "\\unicorn-manipulator-out\\" + out_folder_name
 
 
-def buttonMakeCSV(unicorn_file):
-    # curve_list = app.getAllListBoxes()["curve_list"]
-    curve_list = [x for x in app.getAllCheckBoxes() if app.getAllCheckBoxes()[x] is True]
-    unicorn_file.exportCurves(curve_list, output_folder + "\\curve_out.csv", app.getEntry("resample_size"))
+def resizeArr(arr, size):
+    # Downsamples the array by choosing an even number of points accross the range
+    new_arr = []
+    size = int(size)
+    step = (len(arr) - 1) / float((size - 1))
+    return [arr[int(i * step)] for i in range(size)]
+
+
+def resizeXCoord(x_data, size):
+    # Resizes the points accross the range, assumes it is smalles at the beggining of the array
+    # and the largest value is at the end of the array
+    # ie, it makes a distribution even across the range of x_data
+    start, end = x_data[0], x_data[-1]
+    new_array = [(end - start) / size * i for i in range(size)]
+    return new_array
+
+
+def resizeYCoord(xdata1, ydata1, xdata2):
+    i2 = 0
+    ydata2 = []
+    for i1 in range(1, len(xdata1)):
+        if xdata1[i1] == xdata2[i2]:
+            ydata2.append(ydata1[i1])
+            i2 += 1
+        elif xdata1[i1] > xdata2[i2]:
+            if (xdata1[i1] - xdata1[i1 - 1]) == 0:
+                # if the same point is taken twice, just skip
+                continue
+            else:
+                slope = (ydata1[i1] - ydata1[i1 - 1]) / (xdata1[i1] - xdata1[i1 - 1])
+                new_y = ydata1[i1] - slope * (xdata2[i2] - xdata1[i1])
+                ydata2.append(new_y)
+                i2 += 1
+        if i2 >= len(xdata2):
+            break
+    return ydata2
 
 
 def main():
-    input_files = getFiles()
+    input_files = getInputFiles()
     # print(input_files)
     unicorn_file = UnicornFile(input_files[0])
     unicorn_file.load()
-    global output_folder
-    output_folder = getOutputFolder("out test")
+    unicorn_file.getColumnData()
+    # print(unicorn_file.pycorn_file['Chrom.1_1_True'])
+    keys = unicorn_file.pycorn_file['Chrom.1_1_True'].keys()
+    print(keys)
+    x_data = unicorn_file.pycorn_file['Chrom.1_1_True']['CoordinateData.Volumes']
+    y_data = unicorn_file.pycorn_file['Chrom.1_1_True']['CoordinateData.Amplitudes']
+    # x_coord = resizeXCoord(x_data, 100)
+    # y_coord = resizeYCoord(x_data, y_data, x_coord)
+    # [print(f'{x_coord[i]:.6}, {y_coord[i]:.6}') for i in range(len(x_coord))]
+    x_coord = resizeArr(x_data, 100)
+    # print(len(x_data), len(x_coord))
+    y_coord = resizeArr(y_data, 100)
+    # [print(f'{x_coord[i]:.6}') for i in range(len(x_coord))]
+    [print(f'{x_coord[i]:.6}, {y_coord[i]:.6}') for i in range(len(x_coord))]
 
-    global app
-    app = appJar.gui("Unicorn Curve Exporter")
-    app.startFrame("files", row=0, column=0)
-    app.setStretch("COLUMN")
-    app.addListBox("file_list", [x.split('\\')[-1] for x in input_files])
-    # app.setFrameWidth("files",300)
-    app.setListBoxWidth("file_list", 50)
-    app.setListBoxMulti("file_list", True)
-    app.stopFrame()
-
-    app.startFrame("curves", row=0, column=1)
-    for curve in unicorn_file.curve_names:
-        app.addCheckBox(curve)
-    # app.addListBox("curve_list", unicorn_file.curve_names)
-    # app.setListBoxMulti("curve_list", True)
-    app.stopFrame()
-
-    app.startFrame("data_export", row=0, column=2)
-    app.setStretch("COLUMN")
-
-    app.addMessage("M1", "Select number of points to downsample to:")
-    app.addNumericEntry("resample_size")
-    app.addNamedButton("Make csv", unicorn_file, buttonMakeCSV)
-    app.stopFrame()
-
-    app.go()
-
-    # checked_curves = [x for x in app.getAllCheckBoxes() if app.getAllCheckBoxes()[x] == True]
-    # print(unicorn_file.curve_names.keys())
-    # unicorn_file.getCurveData("pH ", 2000)
-    # unicorn_file.exportCurves(["UV 1_280", "UV 2_254", "Cond", "pH", "DeltaC pressure"], "curve_out.csv", 2000)
-    # print([x.find("EventText").text for x in unicorn_file.blocks["CIP"]])
+    # print(unicorn_file.getCurveData("UV 1_280", x_unit="min", resize=100)[0])
+    # print(unicorn_file.pycorn_file.keys())
+    # print(unicorn_file.pycorn_file["ColumnTypeData"]['Xml'])
+    # print(unicorn_file.pycorn_file["ColumnIndividualData"]["Xml"])
 
 
 if __name__ == '__main__':
